aa815602a559f42459c3c82ecaa678e20147feaf
NACHIKETA <46415457+nachikeat3299@users.noreply.github.com>
2020-09-19 01:47:41 +0900

    텐서보드.py도 추가하여 일단 모든 자동화를 완료함

--- MNIST_eval.py
+++ MNIST_eval.py
@@ -1,36 +1,58 @@
 import tensorflow as tf
-import os
 from tensorflow.examples.tutorials.mnist import input_data
+import os
 
+
+def del_all_flags(FLAGS):
+    flags_dict = FLAGS._flags()
+    keys_list = [keys for keys in flags_dict]
+
+    for keys in keys_list:
+        FLAGS.__delattr__(keys)
+
+# mnist 데이터 가져오기
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True, validation_size=5000)
+
+# Train된 모델들 찾아서 가져오기
+filenames = os.listdir(os.path.join(os.path.curdir, 'runs'))
+filenames.remove('past')
+filenames.remove('failed')
+
+file = open('./log_eval.txt', 'a')
 # 모델이 저장된 checkpoint 경로
-string = str(m.timestamp)
-tf.flags.DEFINE_string("checkpoint_dir", f"./runs/{string}/checkpoints", "Checkpoint directory from training run")
+for filename in filenames:
+    del_all_flags(tf.flags.FLAGS)
+    tf.reset_default_graph()
+    tf.flags.DEFINE_string("checkpoint_dir", f"./runs/{filename}/checkpoints", "Checkpoint directory from training run")
 
-FLAGS = tf.flags.FLAGS
-# ==================================================
-checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir) #가장 validation accuracy가 높은 시점 load
-graph = tf.Graph()
-with graph.as_default():
-    sess = tf.Session()
-    with sess.as_default():
-        saver = tf.train.import_meta_graph(f"{checkpoint_file}.meta")
-        saver.restore(sess, checkpoint_file) # 저장했던 모델 load
+    FLAGS = tf.flags.FLAGS
+    # ==================================================
+    checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir) #가장 validation accuracy가 높은 시점 load
+    graph = tf.Graph()
+    with graph.as_default():
+        sess = tf.Session()
+        with sess.as_default():
+            saver = tf.train.import_meta_graph(f"{checkpoint_file}.meta")
+            saver.restore(sess, checkpoint_file) # 저장했던 모델 load
 
-        # Get the placeholders from the graph by name, name을 통해 operation 가져오기
-        X = graph.get_operation_by_name("X").outputs[0]
-        Y = graph.get_operation_by_name("Y").outputs[0]
-        # TODO *hint* dropout 확률을 placeholder에 추가
-        dropout_prob = tf.placeholder(tf.float32, name="dropout_prob")
+            # Get the placeholders from the graph by name, name을 통해 operation 가져오기
+            X = graph.get_operation_by_name("X").outputs[0]
+            Y = graph.get_operation_by_name("Y").outputs[0]
+            # TODO *hint* dropout 확률을 placeholder에 추가
+            dropout_prob = tf.placeholder(tf.float32, name="dropout_prob")
 
-        hypothesis = graph.get_operation_by_name("hypothesis").outputs[0]
-        correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            hypothesis = graph.get_operation_by_name("hypothesis").outputs[0]
+            correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-        feed_dict = {X:mnist.test.images, Y:mnist.test.labels, dropout_prob: 0.0}
+            feed_dict = {X:mnist.test.images, Y:mnist.test.labels, dropout_prob: 0.0}
 
-        test_accuracy = sess.run(accuracy, feed_dict=feed_dict)
-        print('- Test Max Accuracy:', test_accuracy)
+            test_accuracy = sess.run(accuracy, feed_dict=feed_dict)
+            file.write(f'{filename}\t{test_accuracy}\n')
+            print('- Timestamp        : ', filename)
+            print('- Test Max Accuracy:', test_accuracy)
+    sess.close()
+file.close()
 
 
 
---
+++ tensorboard_run.py
@@ -0,0 +1,24 @@
+import os
+
+def makeDir(_filename):
+    return os.path.join(os.path.curdir, 'runs', _filename)
+
+filenames = os.listdir(os.path.join(os.path.curdir, 'runs'))
+filenames.remove('past')
+filenames.remove('failed')
+
+# tensorboard --logdir=name1:/path/to/logs/1,name2:/path/to/logs/2
+command_names = []
+for filename in filenames:
+    name = f'{filename}'
+    ddir = f'{makeDir(filename)}'
+    name += ':' + ddir
+    command_names.append(name)
+
+command = f'tensorboard --logdir='
+command += ','.join(command_names)
+command += ' --host localhost'
+
+os.system(f'{command}')
+
+
